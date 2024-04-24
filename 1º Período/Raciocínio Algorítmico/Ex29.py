0b8ffc7ab6127ac8c1723c30b7efbad1887d33a3
Marco Agostini <107429726+Marukoalitini@users.noreply.github.com>
2024-04-23 21:04:51 -0300

    Update Ex29.py

--- 1º Período/Raciocínio Algorítmico/Ex29.py
+++ 1º Período/Raciocínio Algorítmico/Ex29.py
@@ -1,3 +1,14 @@
-pratos=int("Digite a quantidade de pratos que serão inseridos o peso: ")
+pratos = int(input("Digite a quantidade de pratos que serão inseridos o peso: "))
+somaprato = 0
+pratos800 = 0
+
 for contador in range(pratos):
-    pesoprato=float()
+    pesoprato = float(input("Digite o peso do prato em gramas: "))
+    somaprato += pesoprato
+    if pesoprato > 800:
+        pratos800 += 1
+
+mediaprato = somaprato / 1000 / pratos
+
+print("A média de peso dos pratos é:", mediaprato, "kg.")
+print("A quantidade de pratos com peso acima de 800 gramas é:", pratos800)
